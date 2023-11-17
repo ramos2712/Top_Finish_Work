0f18c746ee5b2e89976c66b0f757bff9717f0440
lilydias24 <125706749+lilydias24@users.noreply.github.com>
2023-11-17 09:35:18 -0300

    Atualização na Sessão Login

--- Login/login.php
+++ Login/login.php
@@ -4,30 +4,42 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.6/jquery.inputmask.min.js"></script>   
-        <link rel="stylesheet" href="login.css">
         <title>Página de Login</title>
     </HEAD>
-    <BODY class="card-container">
-        <div class="login-card">
-            <div class="card-logo">
-                <a href="#"><img src="#" alt="logo"></a>
-            </div>
-            <div class="card-header">
-                <h1>Entrar</h1>
-                <div>Realize seu login para acessar</div> 
-            </div>
-            <form class="card-form" action="testLogin.php" method="POST">
-                <div class="form-item"> 
-                    CPF : <input type="text" name="CPF" id="CPF" placeholder="Digite seu CPF" required>
+    <BODY style="background-color: #FFDAB9;">          
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header mx-auto text-center" >                            
+                        <a href="#"><img src="../smile.jpg" alt="logo" class="img-fluid mb-3 w-25"></a>
+                        <div class="text-center">
+                            <h3>Entrar</h3>
+                            <h6>Realize seu login para acessar</h6>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form class="card-form text-center" action="testLogin.php" method="POST">
+                            <div class="form-group m-3">
+                                <label for="username">CPF:</label>
+                                <input type="text" class="form-control w-50 mx-auto" name="CPF" id="CPF" placeholder="Digite seu CPF">
+                            </div>
+                            <div class="form-group m-3">
+                                <label for="password">Senha:</label>
+                                <input type="password" class="form-control w-50 mx-auto" name="senha" placeholder="Digite sua senha">
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-3 p-3">Entrar</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="form-item">
-                    Senha : <input type="password" name="senha" placeholder="Digite sua Senha" required>
-                </div>
-                <button class="action_btn" type="submit" name="submit">Entrar</button>
-            </form>
+            </div>
         </div>
+    </div>
+
     </BODY>
     <script>
         $(document).ready(function () {
